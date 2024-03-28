b4bd8ffb5953d4183114a3133ac15bd5524d8eb5
Mirna Garcia <mhannany@gmail.com>
2024-03-27 22:02:28 -0700

    Login feature

--- src/main/java/com/example/moneyM/repository/UserAccountRepository.java
+++ src/main/java/com/example/moneyM/repository/UserAccountRepository.java
@@ -1,11 +1,15 @@
 package com.example.moneyM.repository;
 
 import com.example.moneyM.model.UserAccount;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     // Custom query methods can be defined here if needed e.g.
-    // Optional<UserAccount> findByEmail(String email);
+	  Optional<UserAccount> findByEmail(String email);
+	  Optional<UserAccount> findByUsername(String username);
 }
