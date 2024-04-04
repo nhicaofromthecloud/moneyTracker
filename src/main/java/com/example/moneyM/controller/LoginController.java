d37e8d44c59d9e99da84e8d2f6601a9eb73a7272
nhicaofromthecloud <theresanhi268@gmail.com>
2024-04-03 20:56:24 -0700

    Updated Login controller

--- src/main/java/com/example/moneyM/controller/LoginController.java
+++ src/main/java/com/example/moneyM/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,44 +17,42 @@ import com.example.moneyM.repository.UserAccountRepository;
 import com.example.moneyM.request.UserLoginRequest;
 import com.example.moneyM.response.MessageResponse;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 
 	@Autowired
 	UserAccountRepository userRepository;
-	
-	
+
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody UserLoginRequest loginRequest) {
-		
 		try {
 			String userInfo = loginRequest.getUsername();
 			Optional<UserAccount> userData;
-			
+
 			if (userInfo.contains("@")) {
-			    userData = userRepository.findByEmail(userInfo);
+				userData = userRepository.findByEmail(userInfo);
 			} else {
-			    userData = userRepository.findByUsername(userInfo);
+				userData = userRepository.findByUsername(userInfo);
 			}
-			
+
 			if (userData.isPresent()) {
 				String password = userData.get().getPassword();
-				if (password.equals(loginRequest.getPassword())) {			
-					String userName= userData.get().getUsername();
-					MessageResponse msg = new MessageResponse("Welcome " + userName);
-					return new ResponseEntity<>(msg, HttpStatus.OK);
-				}
-				MessageResponse msg = new MessageResponse("Incorrect password");
-				return new ResponseEntity<>(msg, HttpStatus.FORBIDDEN);
-			}
-			MessageResponse msg = new MessageResponse("Incorrect username");
-			return new ResponseEntity<>(msg, HttpStatus.FORBIDDEN);
-		} catch (Exception e) {
-			MessageResponse msg = new MessageResponse("Server Error");
-			return new ResponseEntity<>(msg, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+				if (password.equals(loginRequest.getPassword())) {
+					UserAccount user = userData.get();
+					user.setPassword(null); // Remove the password for security reasons
 
+					// Return the UserAccount object, excluding the password
+					return new ResponseEntity<>(user, HttpStatus.OK);
+				}
+				return new ResponseEntity<>(new MessageResponse("Incorrect password"), HttpStatus.UNAUTHORIZED);
+			}
+			return new ResponseEntity<>(new MessageResponse("Incorrect username"), HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>(new MessageResponse("Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
-	
+
+
 }
